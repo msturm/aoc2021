78ba2767cf877d02a64bd18c4331a69a792a020a
Martin Sturm <msturm@ebay.com>
2021-12-01 17:38:54 +0100

    Fix stupid mistake

--- 1/1.py
+++ 1/1.py
@@ -3,8 +3,8 @@ file1 = '1.in'
 count1, count2 = 0, 0
 depths = [int(x.strip()) for x in open(file1, 'r').readlines()]
 # super short version
-print("part 1: {0}".format(len([x for x, y in zip(([0] + depths[:-1])[1:], depths[1:]) if x < y])))
-print("part 1: {0}".format(len([x for x, y in zip((depths[:1]*3 + depths[:-3])[3:], depths[3:]) if x < y])))
+print("part 1: {0}".format(len([x for x, y in zip(depths, depths[1:]) if x < y])))
+print("part 2: {0}".format(len([x for x, y in zip(depths, depths[3:]) if x < y])))
 
 
 #original version
