f4a1a5d798ea984810b3232601b7263c2fc2fcb0
Martin Sturm <msturm@ebay.com>
2021-12-01 09:14:16 +0100

    Added 1-line version

--- 1/1.py
+++ 1/1.py
@@ -2,7 +2,12 @@
 file1 = '1.in'
 count1, count2 = 0, 0
 depths = [int(x.strip()) for x in open(file1, 'r').readlines()]
+# super short version
+print("part 1: {0}".format(len([x for x, y in zip(([0] + depths[:-1])[1:], depths[1:]) if x < y])))
+print("part 1: {0}".format(len([x for x, y in zip((depths[:1]*3 + depths[:-3])[3:], depths[3:]) if x < y])))
 
+
+#original version
 for n in range(1, len(depths)):
     if depths[n] > depths[n-1]:
         count1 += 1
