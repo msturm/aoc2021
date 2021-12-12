98cdf157edf39855f9fbd0216d6743b372c9e8ba
Martin Sturm <msturm@ebay.com>
2021-12-12 15:19:56 +0100

    Cleanup day 12

--- 12/12.py
+++ 12/12.py
@@ -4,10 +4,8 @@ file1 = '12.in'
 
 input = [x.strip() for x in open(file1, 'r').readlines()]
 C = {}
-N = []
-V = []
-P = set()
 ans = 0
+ans1 = 0
 for v in input:
     n1, n2 = v.split('-')
     if n1 not in C:
@@ -21,33 +19,25 @@ for v in input:
 print(C)
 
 def visit(cave, path, dbl_sc = ''):
-    global C, V, ans
-    # print(cave)
+    global C, ans, ans1
     if cave != 'end':
-        V.append(cave)
-        smallcaves = [p for p in path if p.islower()]
         for c in C[cave]:
             if c.isupper():
                 visit(c, path + [cave], dbl_sc)
-            elif c.islower() and c not in smallcaves:
-                smallcaves.append(c)
+            elif c.islower() and c not in path:
                 visit(c, path + [cave], dbl_sc)
-            elif c.islower() and c in smallcaves and c != 'start' and c != 'end' and dbl_sc == '':
-                smallcaves.append(c)
+            elif c.islower() and c != 'start' and c != 'end' and dbl_sc == '':
                 visit(c, path + [cave], c)
-            # elif c.islower() and  c in smallcaves and dbl_sc == c:
-            #     smallcaves.append(c)
-            #     visit(c, path + [cave], c)
 
     else:
         ans += 1
-        P.add("".join(path))
-        # print(path + ['end'])
-
+        if not dbl_sc:
+            # part 1
+            ans1 += 1
 
 visit('start', [])
-print(ans)
-print(len(P))
+print("P1", ans1)
+print("P2", ans)
 # P1 26 min 46 sec
 # P2 more than 1,5 hour
 
